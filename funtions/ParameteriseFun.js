09484a85a0efb5f3af9acb2a34aa26daa48ca877
gitshubhsss <shubhamranjane16@gmail.com>
2024-08-19 23:55:44 +0530

    .\ParameteriseFun.js

---
+++ funtions/ParameteriseFun.js
@@ -0,0 +1,12 @@
+function printName(name,age){
+    console.log(`${name}'s age is is ${age}`); 
+}
+
+// printName("amruta",56);
+// printName("sahil",85);
+// printName("barik")//we dont get any eror here it will simply print barik's age is is undefined
+
+function sum(a,b){
+    console.log(a+b); 
+}
+sum(5);
